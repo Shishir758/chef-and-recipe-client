5e9dd56dc0d4857d2863bc1e6859913771f484a2
Shishir758 <studentshishir@gmail.com>
2023-06-22 12:36:50 +0600

    solve firebaseAPI key issue

--- firebase.config.js
+++ firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 
 import.meta.env.VITE_apiKey;
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
+ apiKey: "AIzaSyCSU2Ze6vHl0ieq1W5YVZ5fG7cy4FQhyMg",
+  authDomain: "special-jp-chef.firebaseapp.com",
+  projectId: "special-jp-chef",
+  storageBucket: "special-jp-chef.appspot.com",
+  messagingSenderId: "322721568950",
+  appId: "1:322721568950:web:d771b590891f0d536f6e17"
 };
 
 const app = initializeApp(firebaseConfig);
